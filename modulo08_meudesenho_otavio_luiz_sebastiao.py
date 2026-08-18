ac90b598ab8c833ec6de80877852fcc2351195c8
Aluno Vocação Beatriz <otavioluizsebastiao40@gmail.com>
2026-08-18 16:40:56 -0300

    update_modulo08

---
+++ modulo08_meudesenho_otavio_luiz_sebastiao.py
@@ -0,0 +1,41 @@
+'''
+ben10 - ben10
+coragem o cão covarde - coragem
+steven universo - steven
+'''
+
+
+class BonecoToyStory:
+    def __init__(self, nome, dono, frase_de_efeito):
+        self.nome = nome
+        self.dono = dono
+        self.frase_de_efeito = frase_de_efeito
+
+
+
+senhor_potato = BonecoToyStory(
+           
+nome='Senhor Potato',
+dono='Andy',
+frase_de_efeito='Eu sou o Senhor Batata!'
+)
+
+woody = BonecoToyStory(
+    nome='Woody',
+    dono='Andy',
+    frase_de_efeito='Há uma cobra no meu bota!'
+)
+
+
+buzlaitir = BonecoToyStory(
+    nome='Buzz Lightyear',
+    dono='Andy',
+    frase_de_efeito='Ao infinito e além!'
+)
+
+
+
+
+
+
+
